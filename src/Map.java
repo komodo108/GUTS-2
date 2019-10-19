940d2ec63510bc468a02ac81aaa25d044887f26b
donald108 <__@live.ca>
2019-10-19 15:35:02 +0100

    map working

--- src/Map.java
+++ src/Map.java
@@ -2,16 +2,21 @@ import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PVector;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Map extends PObject {
     enum Direction {
         UP, DOWN, LEFT, RIGHT;
     }
 
     private PImage map;
+    private List<PObject> objects = new ArrayList<>();
 
     Map(PApplet applet) {
         super(applet);
         map = Assets.getInstance().getBackground();
+        pos = new PVector(-map.width / 2f, -map.height / 2f);
     }
 
     @Override
@@ -22,18 +27,19 @@ public class Map extends PObject {
     void move(Direction dir) {
         switch (dir) {
             case UP:
-                pos.x += 10;
+                if(pos.y < -50) pos.y += 40;
                 break;
             case DOWN:
-                pos.x -= 10;
+                if(pos.y > -map.height + 650) pos.y -= 40;
                 break;
             case LEFT:
-                pos.y += 10;
+                if(pos.x < -50) pos.x += 40;
                 break;
             case RIGHT:
-                pos.y -= 10;
+                if(pos.x > -map.width + 850) pos.x -= 40;
                 break;
         }
+        System.out.println(pos);
     }
 
     @Override
